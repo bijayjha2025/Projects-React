f55abbeb8e33f2c8557733ad789535d1b18aaa39
Bijay Jha <bijayjha12345@gmail.com>
2025-12-21 18:02:01 +0545

    styling error message

--- ExpenseTracker/src/Components/ErrorMessage.jsx
+++ ExpenseTracker/src/Components/ErrorMessage.jsx
@@ -1,9 +1,9 @@
 
 function ErrorMessage({message, onClose}){
     return(
-        <div>
-          <span>{message}</span>
-          <button onClick={onClose}></button>
+        <div className="fixed top-6 right-6 bg-red-500 text-white px-6 py-4 rounded-xl shadow-lg flex items-center gap-3 animate-slideIn z-50">
+          <span className="font-medium">{message}</span>
+          <button onClick={onClose} className="text-white hover:text-gray-200 font-bold text-xl">×</button>
         </div>
     );
 }
--- ExpenseTracker/src/Components/ExpenseForm.jsx
+++ ExpenseTracker/src/Components/ExpenseForm.jsx
@@ -44,7 +44,7 @@ function ExpenseForm(){
          <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
             <input type="number" placeholder="Amount" value= {amount} onChange={(e) => setAmount(e.target.value)} className='p-3 bg-gray-50 border border-gray-200 rounded-xl focus:ring-2 focus:ring-amber-400 outline-none transition-all'/>
 
-            <select value={category} onChange={(e) => setCategory(e.target.value)} className='p-3 bg-gray-50 border border-gray-200 rounded-xl focus:ring-2 focus:ring-amber-400 hover: ring-mber-200 outline-none text-gray-600'>
+            <select value={category} onChange={(e) => setCategory(e.target.value)} className='p-3 bg-gray-50 border border-gray-200 rounded-xl focus:ring-2 focus:ring-amber-400 hover:border-amber-200 outline-none text-gray-600'>
                 <option value="">Select category</option>
                 <option value="Food">Food</option>
                 <option value="Transportation">Transportation</option>
@@ -59,7 +59,7 @@ function ExpenseForm(){
              <input type="text" placeholder="Note (optional)" value={note} onChange={(e) => setNote(e.target.value)} className='flex-1 p-3 bg-gray-50 border border-gray-200 rounded-xl focus:ring-2 focus:ring-amber-400 hover:border-amber-200 outline-none placeholder:text-gray-400'/>
             </div>
 
-            <button type="submit" className='mt-4 bg-gradient-to-r from-amber-600 to-amber-700 hover:from-amber-700 hover:to-amber-800 text-white font-bold py-4 rounded-2xl shadow-md transform active:scale-95 transition-all'>Add Expense</button>
+            <button type="submit" className='cursor-pointer mt-4 bg-gradient-to-r from-amber-600 to-amber-700 hover:from-amber-700 hover:to-amber-800 text-white font-bold py-4 rounded-2xl shadow-md transform active:scale-95 transition-all'>Add Expense</button>
          </form>
         </div>
         </>
