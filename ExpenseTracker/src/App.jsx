2a15f85e2b84fde385b58c88e32791ca6b0da8bc
Bijay Jha <bijayjha12345@gmail.com>
2025-12-21 09:18:43 +0545

    Initial setup

---
+++ ExpenseTracker/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+      <div className="max-w-200 m-auto p-1">
+        <h1 className='font-story font-bold'>Expense Tracker</h1>
+      </div>
+  )
+}
+
+export default App
