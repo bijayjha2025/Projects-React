228ace8d59f592c30fe208a863ea247498775569
Bijay Jha <bijayjha12345@gmail.com>
2025-12-28 10:35:43 +0545

    Adding searchbar component

---
+++ RecipeFinder/mithogaas/src/Components/SearchBar.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+
+const SearchBar = ({ onSearch }) => {
+    const [query, setQuery] = useState('');
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            if(query) {
+                onSearch(query);
+            }
+        }, 500);
+
+        return () => clearTimeout(timer);
+    }, [query, onSearch]);
+
+    return(
+        <div className='max-w-2xl mx-auto px-4'>
+            <input type='text' value={query} onChange={(e) => setQuery(e.target.value)} placeholder='Search recipes' className='w-full px-6 py-3 rounded-lg border-gray-300 focus:outline-none focus:border-[#58e633]'/>
+        </div>
+    );
+};
+
+export default SearchBar
